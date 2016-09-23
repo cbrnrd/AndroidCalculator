bd259958aad2f14690dd8c2a10bcc2600241b973
Carter Brainerd <cbawsome77@gmail.com>
2016-09-23 08:43:38 -0400

    Update MainActivity.java

--- AndroidStudioProjects/AndroidCalculator1/app/src/main/java/tic/user/androidcalculator1/MainActivity.java
+++ AndroidStudioProjects/AndroidCalculator1/app/src/main/java/tic/user/androidcalculator1/MainActivity.java
@@ -26,24 +26,24 @@ public class MainActivity extends AppCompatActivity {
         final String pi = Double.toString(Math.PI);
 
 
-        Button button0 = (Button) findViewById(R.id.button0);
-        Button button1 = (Button) findViewById(R.id.button1);
-        Button button2 = (Button) findViewById(R.id.button2);
-        Button button3 = (Button) findViewById(R.id.button3);
-        Button button4 = (Button) findViewById(R.id.button4);
-        Button button5 = (Button) findViewById(R.id.button5);
-        Button button6 = (Button) findViewById(R.id.button6);
-        Button button7 = (Button) findViewById(R.id.button7);
-        Button button8 = (Button) findViewById(R.id.button8);
-        Button button9 = (Button) findViewById(R.id.button9);
-        Button buttonPlus = (Button) findViewById(R.id.buttonPlus);
-        Button buttonPoint = (Button) findViewById(R.id.buttonPoint);
-        Button buttonMinus = (Button) findViewById(R.id.buttonMinus);
-        Button buttonMult = (Button) findViewById(R.id.buttonMult);
-        Button buttonDiv = (Button) findViewById(R.id.buttonDiv);
+        Button button0      = (Button) findViewById(R.id.button0);
+        Button button1      = (Button) findViewById(R.id.button1);
+        Button button2      = (Button) findViewById(R.id.button2);
+        Button button3      = (Button) findViewById(R.id.button3);
+        Button button4      = (Button) findViewById(R.id.button4);
+        Button button5      = (Button) findViewById(R.id.button5);
+        Button button6      = (Button) findViewById(R.id.button6);
+        Button button7      = (Button) findViewById(R.id.button7);
+        Button button8      = (Button) findViewById(R.id.button8);
+        Button button9      = (Button) findViewById(R.id.button9);
+        Button buttonPlus   = (Button) findViewById(R.id.buttonPlus);
+        Button buttonPoint  = (Button) findViewById(R.id.buttonPoint);
+        Button buttonMinus  = (Button) findViewById(R.id.buttonMinus);
+        Button buttonMult   = (Button) findViewById(R.id.buttonMult);
+        Button buttonDiv    = (Button) findViewById(R.id.buttonDiv);
         Button buttonEquals = (Button) findViewById(R.id.buttonEquals);
-        Button buttonClear = (Button) findViewById(R.id.buttonClear);
-        Button buttonMore = (Button) findViewById(R.id.buttonMore);
+        Button buttonClear  = (Button) findViewById(R.id.buttonClear);
+        Button buttonMore   = (Button) findViewById(R.id.buttonMore);
 
         tv = (TextView) findViewById(R.id.textView);
         numView("0");
